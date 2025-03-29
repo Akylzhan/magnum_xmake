3b9648757d80623d217cb23063ea866e8df62d88
Akylzhan <akyl.sauranbai@gmail.com>
2025-03-30 01:41:44 +0500

    add resources

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,68 @@
+#include "Corrade/Containers/ArrayView.h"
+#include "Corrade/Utility/Resource.h"
+#include "Corrade/Utility/Utility.h"
 #include "Magnum/GL/AbstractFramebuffer.h"
+#include <Magnum/GL/Buffer.h>
 #include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/GL/Mesh.h>
 #include <Magnum/Math/Color.h>
 #include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/Shaders/VertexColorGL.h>
 
 #include <absl/numeric/int128.h>
+
+#include <array>
 #include <iostream>
 
 using namespace Magnum;
+using namespace Math::Literals;
 
 class MyApplication : public Platform::Sdl2Application {
+
 public:
   explicit MyApplication(const Arguments &arguments)
-      : Platform::Sdl2Application(arguments) {}
+      : Platform::Sdl2Application(
+            arguments, Configuration{}.setTitle("Magnum Triangle Example")) {
+
+    _mesh.setCount(Containers::arraySize(vertices))
+        .addVertexBuffer(GL::Buffer{vertices}, 0,
+                         Shaders::VertexColorGL2D::Position{},
+                         Shaders::VertexColorGL2D::Color3{});
+  }
+
+private:
   void drawEvent() override {
     GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
     GL::defaultFramebuffer.clearColor(Color3::blue());
+
+    _shader.draw(_mesh);
+
     swapBuffers();
   }
+
+  GL::Mesh _mesh;
+  Shaders::VertexColorGL2D _shader;
+
+  struct TriangleVertex {
+    Vector2 position;
+    Color3 color;
+  };
+
+  // const std::array<TriangleVertex, 3> vertices = {{
+  const TriangleVertex vertices[3] = {
+      {{-0.5f, -0.5f}, 0xff0000_rgbf}, /* Left vertex, red color */
+      {{0.5f, -0.5f}, 0x00ff00_rgbf},  /* Right vertex, green color */
+      {{0.0f, 0.5f}, 0x0000ff_rgbf}    /* Top vertex, blue color */
+  };
+  // }};
 };
 
 int main(int argc, char **argv) {
   std::cout << sizeof(absl::uint128) << "\n";
+
+  Utility::Resource rc{"dummy-resource"};
+  std::cout << std::string{rc.getString("DummyText.txt")} << "\n";
+
   MyApplication app({argc, argv});
   // app.setSwapInterval(1); // VSync
   app.exec();
--- xmake.lua
+++ xmake.lua
@@ -16,20 +16,26 @@ end
 add_repositories("wasm_support https://github.com/Akylzhan/xmake-repo magnum_wasm")
 
 add_requires("magnum feecd28993169c7857701a8d648fca4e2ec50487", { configs = { sdl2 = true, deprecated = false, ldflags = is_plat("wasm") and "-sMIN_WEBGL_VERSION=2" or nil } })
+add_requireconfs("magnum.libsdl2", { configs = { wayland = false } })
 -- https://github.com/mosra/corrade/blob/4ee45ba341febe1744733abd1449460124363c8f/modules/FindCorrade.cmake#L685
-add_requireconfs("magnum.corrade", { override = true, version = "4ee45ba341febe1744733abd1449460124363c8f" })
+add_requireconfs("magnum.corrade", { override = true, version = "4ee45ba341febe1744733abd1449460124363c8f"})
+-- TODO: set dependency on files in resources
+add_requires("corrade 4ee45ba341febe1744733abd1449460124363c8f")
 if is_cross() then
     add_requireconfs("magnum.corrade.corrade", { configs = {toolchains = "clang", runtimes = "c++_shared"} })
+    add_requireconfs("corrade 4ee45ba341febe1744733abd1449460124363c8f", { configs = {toolchains = "clang", runtimes = "c++_shared"} })
 end
-add_requireconfs("magnum.libsdl2", { configs = { wayland = false } })
 
 add_requires("abseil b3b568d90373ed9f75e07c747bc51011557f1b2d", { configs = { cxx_standard = "20" } })
+add_requires("entt", "flatbuffers")
 
 target("magnum_xmake")
     set_languages("c++20")
     set_kind("binary")
-    add_files("src/*.cpp")
-    add_packages("magnum", "abseil")
+    add_packages("magnum", "abseil", "entt", "flatbuffers", "corrade")
+    add_rules("@corrade/resource")
+    add_files("src/*.cpp", "assets/resources/*.conf")
+
     set_warnings("allextra", "error", "pedantic")
     add_cxxflags("-Wthread-safety", "-Wunsafe-buffer-usage", "-fsafe-buffer-usage-suggestions")
     if is_plat("linux") then
